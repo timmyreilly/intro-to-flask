3a023596da19dd3a48bba87513a18a45a160a2e6
TimmyReilly <timreilly@live.com>
2016-06-17 14:05:32 -0700

    added resume page

--- hello.py
+++ hello.py
@@ -1,11 +1,14 @@
 #app.py 
-
 from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
