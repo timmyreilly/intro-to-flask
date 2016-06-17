fe518935dbe6d6fbbc38b74b62bc121df4a550b6
TimmyReilly <timreilly@live.com>
2016-06-17 14:54:30 -0700

    templates again

--- hello.py
+++ hello.py
@@ -1,14 +1,30 @@
 #app.py 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for 
+from flask.ext.wtf import Form 
+from wtforms import StringField, SubmitField
 from flask.ext.bootstrap import Bootstrap
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess'
 
 bootstrap = Bootstrap(app)
+
+class SearchForm(Form):
+    searchterm = StringField('What would you like to search?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/search", methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        session['searchterm'] = form.searchterm.data 
+        return redirect(url_for('search'))
+    return render_template('search.html', form=form, searchterm=session.get('searchterm'))
 
 if __name__ == "__main__":
     app.run(debug=True)
