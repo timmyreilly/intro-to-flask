3e079f0361f9ad2ec1d6c5f08a3d0a6ce5f83845
TimmyReilly <timreilly@live.com>
2016-06-17 17:05:51 -0700

    finished simple

--- hello.py
+++ hello.py
@@ -5,6 +5,8 @@ from wtforms import StringField, SubmitField
 from flask.ext.bootstrap import Bootstrap
 from wtforms.validators import Required
 
+from wiki_sentiment import * 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess'
 
@@ -23,8 +25,18 @@ def search():
     form = SearchForm()
     if form.validate_on_submit():
         session['searchterm'] = form.searchterm.data 
+        session['sentiment'] = get_sentiment_from_url(session.get('searchterm'))
         return redirect(url_for('search'))
-    return render_template('search.html', form=form, searchterm=session.get('searchterm'))
+    return render_template('search.html', form=form, searchterm=session.get('searchterm'), number=session.get('sentiment'))
+
+# @app.route("/search", methods=['GET', 'POST'])
+# def search():
+#     form = SearchForm()
+#     if form.validate_on_submit():
+#         session['searchterm'] = form.searchterm.data 
+#         sentiment = get_sentiment_from_url(session.get('searchterm'))
+#         return redirect(url_for('search'))
+#     return render_template('search.html', form=form, searchterm=session.get('searchterm'), number=sentiment)
 
 if __name__ == "__main__":
     app.run(debug=True)
